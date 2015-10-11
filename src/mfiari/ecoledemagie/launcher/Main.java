156ae13f7b338a948a2b95629c7d51d18edbd32a
mfiari <fiarimike@yahoo.fr>
2015-10-11 22:20:02 +0200

    modif

--- src/mfiari/ecoledemagie/launcher/Main.java
+++ src/mfiari/ecoledemagie/launcher/Main.java
@@ -5,12 +5,6 @@
  */
 package mfiari.ecoledemagie.launcher;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
  * @author mike
@@ -18,22 +12,8 @@ import java.util.logging.Logger;
 public class Main {
     
     public static void main (String args[]) {
-        try {
-            Runtime rt = Runtime.getRuntime();
-            //Process pr = rt.exec("java -jar exe/EcoleDeMagieGame.jar debug launcher");
-            //Process pr = rt.exec("cmd /c dir exe");
-            //Process pr = rt.exec("cmd /c java -jar exe/EcoleDeMagieGame.jar");
-            Process pr = rt.exec("java -cp exe/EcoleDeMagieGame.jar mfiari.ecoledemagie.game.jeu");
-            BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-            String line;
-            while((line=input.readLine()) != null) {
-                System.out.println(line);
-            }
-            int exitVal = pr.waitFor();
-            System.out.println("Exited with error code "+exitVal);
-        } catch (IOException | InterruptedException ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Launcher launcher = new Launcher();
+        launcher.launch();
     }
     
 }
